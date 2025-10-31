ef106915d79c4ebae2bf38cec8e4ffba8fe25bbc
techboy101-kossi <techtompson@gmail.com>
2025-10-31 18:41:29 +0100

    add a new code input

--- Frontend/src/redux/api.js
+++ Frontend/src/redux/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const baseURL = 'http://localhost:8080/api';
+// const baseURL = 'http://localhost:8080/api';
+const baseURL = "https://note-app-backend-gamma.vercel.app/api";
 
 const API = axios.create({ baseURL: baseURL });
 
